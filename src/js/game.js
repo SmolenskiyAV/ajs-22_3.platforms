729639276fe57199a56a3afa4e437278179de38d
Smolenskiy Anton <JoJo_666@list.ru>
2022-02-09 23:32:12 +0700

    Версия 0.4

---
+++ src/js/game.js
@@ -0,0 +1,18 @@
+import Character from "./domain";
+export default class Game {};
+export {readGameSaving, writeGameSaving};
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
